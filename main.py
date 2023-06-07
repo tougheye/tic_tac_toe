2d133a7e82cc11a0832c0c33a74193d317b68a3a
tafaeemp3 <123840362+tafaeemp3@users.noreply.github.com>
2023-06-07 11:57:55 -0700

    update main.py

--- main.py
+++ main.py
@@ -4,12 +4,10 @@ from ext_classes import Player, Game
 player_1 = Player('X')
 player_2 = Player('O')
 
+# The game grid size can be adjusted
 grid_size = input('Please enter the grid size: ')
 
 game = Game(player_1, player_2, grid_size=grid_size)
-game.start_game()
-
-
-
+game.start_game()           #to start the game by turning game_on = True
 
 game.mainloop()
